ba37fc896ce94281a7146d3efc60c93f7d5c2214
Egon Elbre <egonelbre@gmail.com>
2018-03-16 10:50:30 +0200

    Add more columns.

--- export-emoji.bat
+++ export-emoji.bat
@@ -1,11 +1,11 @@
 @set ASESPRITE=f:\Games\Steam\steamapps\common\Aseprite\Aseprite.exe
 
-%ASESPRITE% -b icon/emoji.ase -scale 3 --format json-array --sheet .thumb/icon/emoji-3x.png --data ~rendered\emoji-3x.json --sheet-type rows -sheet-width 520
+%ASESPRITE% -b icon/emoji.ase -scale 3 --format json-array --sheet .thumb/icon/emoji-3x.png --data ~rendered\emoji-3x.json --sheet-type rows -sheet-width 672
 go run twitterify.go .thumb/icon/emoji-3x.png ~rendered\emoji-3x-twitter.png
 
 %ASESPRITE% -b icon/emoji.ase -scale 3 --save-as ~rendered/emoji-3x/gopher-{tag}-{frame}.png
 
-%ASESPRITE% -b icon/emoji.ase --format json-array --sheet icon/emoji.png --data icon/emoji.json --sheet-type rows -sheet-width 160
+%ASESPRITE% -b icon/emoji.ase --format json-array --sheet icon/emoji.png --data icon/emoji.json --sheet-type rows -sheet-width 224
 
 %ASESPRITE% -b icon/emoji.ase --save-as icon/emoji/gopher-{tag}.png{frame}
 
